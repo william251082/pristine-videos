13bc9a1ac8bc7050d92a185fb8eae397ba569e7a
william <william@magicmanager.nl>
2022-04-03 10:09:02 +0200

    Card Component Error Handling

--- components/core/Card/Card.tsx
+++ components/core/Card/Card.tsx
@@ -1,25 +1,30 @@
-import {FC} from "react";
+import {FC, useState} from "react";
 import styles from "./Card.module.css";
 import {CardProps, ClassMapImgSizes} from "./CardTypes";
 import Image from "next/image";
 
-const Card: FC<CardProps> = ({imgUrl, size, id, shouldScale}) => {
+const Card: FC<CardProps> = ({imgUrl, size}) => {
+    const [imgSrc, setImgSrc] = useState(imgUrl);
     const classMap: ClassMapImgSizes = {
         large: styles.lgItem,
         medium: styles.mdItem,
         small: styles.smItem
     };
+    const handleOnError = () => {
+        setImgSrc("/static/clifford.webp")
+    }
     return (
         <div className={styles.container}>
             Card
             <div className={classMap[size]}>
                 <Image
                     className={styles.cardImg}
-                    src={imgUrl}
+                    src={imgSrc}
                     alt="image"
                     layout="fill"
                     width="300px"
                     height="300px"
+                    onError={handleOnError}
                 />
             </div>
         </div>
