a7276b098a00dfdd58dff6798be8802a58130c77
william <william@magicmanager.nl>
2022-04-04 21:32:56 +0200

    Error Handling for Video Library

--- lib/videos.ts
+++ lib/videos.ts
@@ -36,18 +36,28 @@ export interface VideoItem {
     }
 }
 
-export const getVideos = async (searchQuery: string): Promise<DisneyVideo[]> => {
+export const getVideos = async (searchQuery: string) => {
     const YOUTUBE_API_KEY: string | undefined = process.env.YOUTUBE_API_KEY
-    const response = await fetch(
-        `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${searchQuery}&key=${YOUTUBE_API_KEY}`
-    )
-    const data = await response.json()
+    try {
+        const response = await fetch(
+            `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${searchQuery}&key=${YOUTUBE_API_KEY}`
+        )
+        const data = await response.json()
 
-    return data.items.map((item: VideoItem) => {
-        return {
-            id: item.id.videoId,
-            title: item.snippet.title,
-            imgUrl: item.snippet.thumbnails.high.url
+        if (data.error) {
+            console.error('Youtube API error.', data.error)
+            return []
         }
-    })
+
+        return data.items.map((item: VideoItem) => {
+            const id = item.id.videoId || item.id
+            return {
+                id,
+                title: item.snippet.title,
+                imgUrl: item.snippet.thumbnails.high.url
+            }
+        })
+    } catch (error) {
+        console.error('Something went wrong with the video library', error)
+    }
 }
