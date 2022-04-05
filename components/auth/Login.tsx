7db2e545adecde83621f93befd2e0dc8602da814
william <william@magicmanager.nl>
2022-04-05 20:09:07 +0200

    Sign-in Header Component

--- components/auth/Login.tsx
+++ components/auth/Login.tsx
@@ -1,14 +1,33 @@
 import {FC} from "react";
 import Head from "next/head";
+import Image from "next/image";
+import Link from 'next/link';
+import styles from "./Login.module.css";
 
 const Login: FC = () => {
-  return (
-    <div>
-      <Head>
-        <title>Pristine Videos SignIn</title>
-      </Head>
-    </div>
-  )
+    return (
+        <div>
+            <Head>
+              <title>Pristine Videos SignIn</title>
+            </Head>
+            <header className={styles.header}>
+                <div className={styles.headerWrapper}>
+                    <Link href="/">
+                        <a className={styles.logoLink}>
+                            <div className={styles.logoWrapper}>
+                                <Image
+                                    src="/static/netflix.svg"
+                                    alt="Netflix logo"
+                                    width="128px"
+                                    height="34px"
+                                />
+                            </div>
+                        </a>
+                    </Link>
+                </div>
+            </header>
+        </div>
+    )
 }
 
 export default Login
