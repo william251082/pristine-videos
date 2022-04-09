f52ccdfb2f95251b391e8bc1e8250ea2991ab0cf
william <william@magicmanager.nl>
2022-04-09 20:40:45 +0200

    Implement Youtube API Player & Docs

--- pages/video/[videoId].tsx
+++ pages/video/[videoId].tsx
@@ -2,6 +2,8 @@ import {useRouter} from "next/router"
 import Modal from "react-modal"
 import styles from "@styles/Video.module.css";
 
+Modal.setAppElement("#__next");
+
 const Video = () => {
     const router = useRouter()
     const videoId = router.query.videoId
@@ -13,7 +15,12 @@ const Video = () => {
                 onRequestClose={() => router.back()}
                 overlayClassName={styles.overlay}
             >
-                <div>Modal Body</div>
+                <iframe id="player"
+                        width="640"
+                        height="390"
+                        src={`https://www.youtube.com/embed/${videoId}?enablejsapi=1&origin=https://example.com&controls=0`}
+                        frameBorder="0"
+                />
             </Modal>
         </div>
     )
