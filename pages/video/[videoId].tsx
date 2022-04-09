a72eb6653e2f4da5067fc7220f9479a257137f5f
william <william@magicmanager.nl>
2022-04-09 22:13:06 +0200

    Implement Incremental Static Regeneration (ISR)

--- pages/video/[videoId].tsx
+++ pages/video/[videoId].tsx
@@ -1,21 +1,45 @@
 import {useRouter} from "next/router"
 import Modal from "react-modal"
-import styles from "@styles/Video.module.css";
-import cn from "classnames";
+import styles from "@styles/Video.module.css"
+import cn from "classnames"
+import {InferGetStaticPropsType} from "next";
 
-Modal.setAppElement("#__next");
+Modal.setAppElement("#__next")
 
-const Video = () => {
-    const router = useRouter()
-    const id = router.query.videoId
-    const video = {
-        id,
+interface VideoProps {
+    title: string
+    publishTime: string
+    description: string
+    channelTitle: string
+    viewCount: number
+}
+
+export async function getStaticProps() {
+    const video: VideoProps = {
         title: 'Cute dog',
-        publishTime: '1990-0101',
+        publishTime: '1990-01-01',
         description: 'A big red dog',
         channelTitle: 'Paramount',
         viewCount: 1000
     }
+
+    return {
+        props: { video },
+        revalidate: 10
+    };
+}
+
+export async function getStaticPaths() {
+    const listOfVideos = ["mYfJxlgR2jw", "4zH5iYM4wJo", "KCPEHsAViiQ"]
+    const paths = listOfVideos.map((videoId) => ({
+        params: { videoId }
+    }))
+
+    return { paths, fallback: "blocking" }
+}
+
+const Video = ({video}: InferGetStaticPropsType<typeof getStaticProps>) => {
+    const router = useRouter()
     const {title, publishTime, description, channelTitle, viewCount} = video
     return (
         <div className={styles.container}>
@@ -29,7 +53,7 @@ const Video = () => {
                         className={styles.videoPlayer}
                         width="100%"
                         height="390"
-                        src={`https://www.youtube.com/embed/${id}?enablejsapi=1&origin=https://example.com&controls=0`}
+                        src={`https://www.youtube.com/embed/mYfJxlgR2jw?enablejsapi=1&origin=https://example.com&controls=0`}
                         frameBorder="0"
                 />
                 <div className={styles.modalBody}>
