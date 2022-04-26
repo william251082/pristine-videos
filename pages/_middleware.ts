f04ed7a0b83c6a48fc416d539d4759454eb39122
william <william@magicmanager.nl>
2022-04-26 18:46:44 +0200

    Middleware fix

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,12 +1,10 @@
 import {NextFetchEvent, NextRequest, NextResponse} from "next/server";
-import {verifyToken} from "@utils/verifyToken";
 
 export async function middleware(req: NextRequest, ev: NextFetchEvent) {
     const token = req ? req.cookies?.token : ''
-    const userId = await verifyToken(token)
     const {pathname, origin} = req.nextUrl.clone()
 
-    if ((token && userId) || userId || pathname.includes(`/api/login`) || pathname.includes('/static')) {
+    if (token || pathname.includes(`/api/login`) || pathname.includes('/static')) {
         return NextResponse.next()
     }
     if (!token && pathname !== `/login`) {
